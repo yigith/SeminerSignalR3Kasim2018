b588c7227cfa2711c550431b4469a8b49420cb8e
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2018-11-03 17:13:54 +0300

    first commit

---
+++ SeminerSignalR/Hubs/BallHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeminerSignalR.Hubs
+{
+    public class BallHub : Hub
+    {
+        public void Move(int left, int top)
+        {
+            Clients.Others.moveBall(left, top);
+        }
+    }
+}
---
+++ SeminerSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeminerSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string sender, string message)
+        {
+            Clients.Others.broadcastMessage(sender, message);
+        }
+    }
+}
